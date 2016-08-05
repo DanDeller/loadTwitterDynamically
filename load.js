6d46d79768da2c6f1e3620e32ce6fd72ce099ab0
Dan Deller <deller26@gmail.com>
2016-08-05 15:40:51 -0700

    Create load.js

---
+++ load.js
@@ -0,0 +1,66 @@
+    var grabStars = '';
+
+    $('.slides li img').on('click', function() {
+
+      var deviceID   = $(this).attr('data-id'),
+          dataSlug   = $(this).parents('li').find('.get-slug').attr('data-slug'),
+          twitterURL = $(this).attr('data-url');
+          grabStars  = $(this).parents('li').find('.grab-stars').text().length;
+
+      $('.twitter-timeline').attr('href', twitterURL);
+
+      $('.load-wrap').fadeIn(500);
+
+      $('.info-blurb').fadeOut(500);
+
+      $('.fade-it').fadeOut(500, function() {
+
+        $.ajax({
+          url: '/api/device/' + deviceID
+        }).done(function(data) {
+          $('.company-image').attr('src', 'https://s3-us-west-1.amazonaws.com/wearablezone/uploads/' + data.company.logo);
+
+          $('.company-name').text(data.name);
+
+          $('.company-info').html(data.company.body);
+
+          $('.quick-excerpt').text(data.company.short_description);
+          
+          $('.full-story a').attr('href', 'https://wearablezone.com/device/' + dataSlug);
+
+          $('.stock-name').text(data.company.name);
+
+          loadScript(myPrettyCode);
+          myPrettyCode(data.company.twitter_name);
+
+          $('.up-top, .full-story, up-top, .full-story-rate, .company-image').removeClass('hide');
+
+          $('.fade-it').fadeIn(500);
+
+          $('.load-wrap').fadeOut(500);
+        }); // end ajax call
+
+      }); // end fadeOut/In
+
+    }); // end on click
+
+    function loadScript(callback) {
+      var head = document.getElementsByTagName('head')[0],
+          script = document.createElement('script');
+
+      script.type = 'text/javascript';
+      script.src="//platform.twitter.com/widgets.js";
+      
+      // bind the event to the callback function
+      script.onreadystatechange = callback;
+      script.onload = callback;
+
+      $('.load-here').append(script);
+    }
+
+    var myPrettyCode = function(handle) {
+      var handleBox = $('.twitter-hold');
+      $('.load-here').html(
+        '<a id="twitter-feed" class="twitter-timeline" data-tweet-limit=3 href="' + handle + '"></a> '
+        );
+    }
